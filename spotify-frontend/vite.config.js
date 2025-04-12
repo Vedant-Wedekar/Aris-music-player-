6e059a20adae4534c751b2bb18865188904b726e
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-04-12 08:59:51 +0530

    Add initial frontend setup with React, Vite, Tailwind CSS, and essential assets

---
+++ spotify-frontend/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // Enable dark mode
+  plugins: [],
+}
