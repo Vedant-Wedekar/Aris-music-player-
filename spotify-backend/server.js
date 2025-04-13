929cb37dfe2c9f80debfd75e9c2fe116fbe7eb7c
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-04-13 20:52:26 +0530

    refactor: reorganize server.js structure and remove unused App.css styles

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -2,31 +2,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const fs = require('fs');
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Ensure 'uploads' folder exists
+if (!fs.existsSync('uploads')) {
+  fs.mkdirSync('uploads');
+}
 app.use('/uploads', express.static('uploads'));
-// Routes
-const authRoutes = require('./routes/auth');
-app.use('/auth', authRoutes);  // 👈 important
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/songs', require('./routes/songs'));
 app.use('/api/playlists', require('./routes/playlist'));
 
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-
-
-
-
